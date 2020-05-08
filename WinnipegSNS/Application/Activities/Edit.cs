5a23856061288f742cf314638bb1540dc2cb5d35
hank.dohwankim <hank.dohwankim@gmail.com>
2020-05-07 23:13:33 -0500

    List, Edit class added

---
+++ WinnipegSNS/Application/Activities/Edit.cs
@@ -0,0 +1,52 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using WinnipegSNS.Data;
+
+namespace WinnipegSNS.Application.Activities
+{
+    public class Edit
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+            public string Title { get; set; }
+            public string Descr { get; set; }
+            public string Category { get; set; }
+            public DateTime Date { get; set; }
+            public string City { get; set; }
+            public string Street { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly ApplicationDbContext _db;
+            public Handler(ApplicationDbContext db)
+            {
+                _db = db;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _db.Activities.FindAsync(request.Id);
+
+                if(activity == null)
+                {
+                    throw new Exception("Could not find activity");
+                }
+
+                activity.Title = request.Title ?? activity.Title;
+
+                var success = await _db.SaveChangesAsync() > 0;
+
+                if (success) return Unit.Value;
+
+                throw new Exception("Problem saving changes");
+            }
+        }
+    }
+}
